6860d45196dbefbaccc5c8341e494c8b6ab5da5c
Hackbright Student <user@mini-003.(none)>
2013-10-21 17:55:56 -0700

    first commit

--- app.py
+++ app.py
@@ -1,20 +1,59 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 import model
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("username"):
+        return "%s is logged in!"%session['username']
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    user_id = model.authenticate(username, password)
+    if username != None:
+        flash("User authenticated!")
+        session['user_id'] = user_id
+        session['username'] = username
+    else:
+        flash("Password incorrect, there may be a ferret stampede in progress!")
+    
+    return redirect(url_for("index"))#render_template("login.html")
+
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/logout")
+def clear_session():
+    session.clear()
+    return render_template('index.html')
+
+@app.route("/user/<username>")
+def view_user(username):
+    user_id = model.get_user_by_name(username)
+    wall_posts = model.get_wall_posts_by_id(user_id)
+    author_name = model.get_name_by_id(session["user_id"])
+    return render_template('wall.html', wall_posts=wall_posts,
+                                        username=username,
+                                        author_name=author_name)
+
+# owner_id, author_id, created_at, content
+@app.route("/user/<username>", methods=["POST"])
+def make_new_post(username):
+    owner_id = model.get_user_by_name(username)
+    author_id = model.get_user_by_name(session["username"])
+    created_at = datetime.now()
+    content = request.form.get("wall_post")
+    model.make_wall_post(owner_id, author_id, created_at, content)
+    return redirect(url_for("view_user", username=username))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,5 +1,71 @@
-ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+# ADMIN_USER="hackbright"
+# ADMIN_PASSWORD = 5980025637247534551
+import sqlite3
+
+DB = None
+CONN = None
 
 def authenticate(username, password):
-    return False
+    connect_to_db()
+    query = """SELECT id FROM Users WHERE username = ? and password = ?"""
+    DB.execute(query, (username, hash(password),))
+    row = DB.fetchone()
+    CONN.close()
+    if row[0]:
+        return row[0]
+    else:
+        return None
+
+def get_user_by_name(username):
+    connect_to_db()
+    query = """SELECT id FROM Users WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+    CONN.close()
+    if row:
+        return row[0]
+    else:
+        return None
+
+def get_name_by_id(user_id):
+    connect_to_db()
+    query = """SELECT username FROM Users WHERE id = ?"""
+    DB.execute(query, (user_id,))
+    row = DB.fetchone()
+    CONN.close()
+    if row:
+        return row[0]
+    else:
+        return None
+
+def get_wall_posts_by_id(user_id):
+    connect_to_db()
+    query = """SELECT * from wall_posts where owner_id = ?"""
+    # id, owner_id, author_id, created_at, content
+    DB.execute(query, (user_id,))
+    rows = DB.fetchall()
+    CONN.close()
+    if rows:
+        return rows
+    else:
+        return None
+
+def make_wall_post(owner_id, author_id, created_at, content):
+    connect_to_db()
+    query = """INSERT into wall_posts (owner_id, author_id, created_at, content) values(?, ?, ?, ?)"""
+    DB.execute(query, (owner_id, author_id, created_at, content,))
+    CONN.commit()
+    CONN.close()
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
+
+
+def main():
+    connect_to_db()
+    CONN.close()
+
+if __name__ == "__main__":
+    main()
